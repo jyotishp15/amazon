fb3a40b21681032ec7ff1a4e6159da033a0d7a25
Jyotish Phukon <Jyotish Phukon>
2016-11-13 10:49:35 +0530

    message

---
+++ src/test/java/com/home/loginTest/LoginTest.java
@@ -0,0 +1,12 @@
+package com.home.loginTest;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test(enabled=false)
+	public void verifyLoginWithVaildCredential(){
+		
+	}
+
+}
